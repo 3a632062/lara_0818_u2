5f5fd2e1249c0476be671e71d72d81536ce65fd9
lai-chun-lin <axd5201211@gmail.com>
2019-10-29 16:37:04 +0800

    add hellocontroller code

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     //
+	public function index($name = 'Everybody')
+	{
+		$data = ['name' => $name];
+		return view('hello.index', $data);
+	}
 }
